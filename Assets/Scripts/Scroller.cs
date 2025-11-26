56377ae1f24591f35a1756a3bafb8410bccd0cfb
lrees010 <lrees010@odu.edu>
2025-11-26 02:41:10 -0500

    [FEAT] Background changes based on level

--- Assets/Scripts/Scroller.cs
+++ Assets/Scripts/Scroller.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,8 +9,41 @@ public class Scroller : MonoBehaviour
 
     [SerializeField] private RawImage _img;
     [SerializeField] private float speed;
+
+    public Texture Forest;
+    public Texture Cave;
+
     void Update()
     {
         _img.uvRect = new Rect(_img.uvRect.position + new Vector2(0,speed) * Time.deltaTime,_img.uvRect.size);
+    }
+
+    void Start()
+    {
+        EventManager.Subscribe("OnLevelChanged", ChangeBG); //i.e "Forest" "Cave"
+    }
+
+    private void OnDestroy()
+    {
+        EventManager.Unsubscribe("OnLevelChanged", ChangeBG);
+    }
+
+
+
+    private void ChangeBG(object data)
+    {
+        string LevelName = data.ToString();
+
+
+        switch (LevelName) //Maybe change this
+        {
+            case "Forest":
+                _img.texture = Forest;
+                break;
+
+            case "Cave":
+                _img.texture = Cave;
+                break;
+        }
     }
 }
